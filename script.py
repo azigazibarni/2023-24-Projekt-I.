de650ca902550375bee5b412cbb72a2113f01d63
Szlonkai Benedek <szlonkaibenedek@gmail.com>
2024-02-13 16:03:45 +0100

    binary to decimal != False

--- script.py
+++ script.py
@@ -7,11 +7,10 @@ destInput = int(input('Add meg a cél számrendszert (2, 8, 10, 16): '))
 def decimal(num, sys):
     if sys == 2:
         out = 0
-        num = [num[::-1]]
-        for i in len(num):
-            if num[i] == 1:
+        num = list(num[::-1])
+        for i in range(len(num)):
+            if int(num[i]) == 1:
                 out += 2**i
-        return out
 
     elif sys == 8:
         print(':3')
@@ -23,5 +22,7 @@ def decimal(num, sys):
         print(':3')
     else:
         return
+    
+    return out
 
-print(decimal(numInput, 2))
+print(decimal(numInput, sys=2))
