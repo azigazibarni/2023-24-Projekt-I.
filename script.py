54aa6e839ca20df4a055de880e09bf4d4a48baff
Szlonkai Benedek <szlonkaibenedek@gmail.com>
2024-02-13 12:33:21 +0000

    kexet

--- script.py
+++ script.py
@@ -1,7 +1,29 @@
+from tkinter import *
+
+window = Tk()
 def HelloWorld(str):
     print(str)
 
 HelloWorld('print')
+sysInput = int(input('Add meg az átváltandó számrendszert (2, 8, 10, 16): '))
+numInput = str(input('Add meg az átváltandó számot: '))
+destInput = int(input('Add meg az átváltandó számrendszert (2, 8, 10, 16): '))
 
+# sys: 2, 8, 10, 16
 
-def 
+def tizesbe(num, sys):
+    if sys == 2:
+        print(':3')
+
+    elif sys == 8:
+        print(':3')
+
+    elif sys == 10:
+        print(':3')
+
+    elif sys == 16:
+        print(':3')
+    else:
+        return
+
+window.mainloop()
